c375ec135717e934a7e86d1cdff3f1eb45aa32b0
lee-wood <leemaewood@gmail.com>
2020-07-31 23:28:31 -0400

    code comments

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,14 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        oldest = self.storage[0]
+        #we need to id the oldest item and replace it when the ring is full.
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.storage.pop(0) #removes from front
+        self.storage.append(item) #adds to end
 
     def get(self):
-        pass
+        return self.storage
